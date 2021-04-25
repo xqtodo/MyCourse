79b70152b334125121a23f6b97ad27aad7e0220e
xqtodo <2220230039@qq.com>
2021-04-25 22:46:19 +0800

    stream流简单例子

--- src/com/atguigu/replenish/foreach.java
+++ src/com/atguigu/replenish/foreach.java
@@ -11,6 +11,6 @@ public class foreach {
     public static void main(String[] args) {
         List<String> stringList = Arrays.asList("ni","hao","wa");
         /*遍历stringList集合，依次将集合中的变量拿出来存入变量list中然后打印list*/
-        stringList.forEach(list -> System.out.println(list));
+        stringList.forEach(System.out::println);
     }
 }
